d206b3f3f623ef14fc8b25499e1b81ec60c9381e
MartinRangel16 <114451321+MartinRangel16@users.noreply.github.com>
2025-04-10 22:32:58 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-import { SUPABASE_URL, SUPABASE_ANON_KEY } from './supabase-config.js';
+import { SUPABASE_URL, SUPABASE_ANON_KEY } from '.supabase-config.js';
 
 const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
